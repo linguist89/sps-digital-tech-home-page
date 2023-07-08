10b9c78543f3a64d734f51ec836d707f5b9db620
stephan <smuts1989@gmail.com>
2023-07-08 18:40:37 +0200

    Added a divider to show products

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './Header';
 import Footer from './Footer';
 import BodyComponent from './BodyComponent';
 import Products from './Products';
+import Divider from './Divider';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <BodyComponent></BodyComponent>
+      <Divider></Divider>
       <Products></Products>
       <div style={{height: '400px'}}></div>
       <Footer></Footer>
---
+++ src/Divider.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Divider.css';
+
+function Divider(){
+    return (
+        <div className="split-bg">
+            <h1 className="split-color-text">Products</h1>
+        </div>
+    )
+}
+
+export default Divider;
